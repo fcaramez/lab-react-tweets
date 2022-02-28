bbf69bba47077f28e21c656da3642e15e264b88d
fcaramez <francisco.caramez.duarte@gmail.com>
2022-02-28 16:11:34 +0000

    lab done

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function User(props) {
+  return (
+    <div className="user">
+      <span className="name"> {props.name} </span>
+      <span className="handle"> {props.handle} </span>
+    </div>
+  );
+}
+
+export default User;
